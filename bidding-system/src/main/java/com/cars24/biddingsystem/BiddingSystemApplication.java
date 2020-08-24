de2504966677db934af70da577be502c78e24f88
Piyush Goyal <pg.wiprotech@gmail.com>
2020-08-24 23:32:20 +0530

    main entry of bidding system app code added

--- bidding-system/src/main/java/com/cars24/biddingsystem/BiddingSystemApplication.java
+++ bidding-system/src/main/java/com/cars24/biddingsystem/BiddingSystemApplication.java
@@ -2,8 +2,11 @@ package com.cars24.biddingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@ServletComponentScan
 public class BiddingSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BiddingSystemApplication.class, args);
