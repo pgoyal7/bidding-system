b832c01ce1716e44a2787ef007738df4ace78028
Piyush Goyal <pg.wiprotech@gmail.com>
2020-08-24 23:31:47 +0530

    Service and related repository classed added

--- bidding-system/src/main/java/com/cars24/biddingsystem/repository/AuctionDetailHistoryRepository.java
+++ bidding-system/src/main/java/com/cars24/biddingsystem/repository/AuctionDetailHistoryRepository.java
@@ -1,10 +1,9 @@
 package com.cars24.biddingsystem.repository;
 
-import com.cars24.biddingsystem.entity.UserAuctionInfo;
-import org.springframework.data.repository.CrudRepository;
+import com.cars24.biddingsystem.entity.AuctionDetailHistory;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserAuctionInfoRepository extends PagingAndSortingRepository<UserAuctionInfo, Long> {
+public interface AuctionDetailHistoryRepository extends PagingAndSortingRepository<AuctionDetailHistory, Long> {
 }
