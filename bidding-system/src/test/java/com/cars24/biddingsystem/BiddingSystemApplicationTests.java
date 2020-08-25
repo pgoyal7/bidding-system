5aa8b943a09e45e3999396b28e2574465b5c9992
Piyush Goyal <pg.wiprotech@gmail.com>
2020-08-25 19:55:49 +0530

    Test cases and specs added coresponds to bidding system

--- bidding-system/src/test/java/com/cars24/biddingsystem/BiddingSystemApplicationTests.java
+++ bidding-system/src/test/java/com/cars24/biddingsystem/BiddingSystemApplicationTests.java
@@ -1,13 +1,14 @@
 package com.cars24.biddingsystem;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-class BiddingSystemApplicationTests {
-
+public class BiddingSystemApplicationTests {
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
-
 }
