b0aa3fd4f1563bd85e7bfca3a2ee66a7c47bf2a2
Piyush Goyal <pg.wiprotech@gmail.com>
2020-08-25 13:03:29 +0530

    Changes related to message and other cosmetic changes done

--- bidding-system/src/main/java/com/cars24/biddingsystem/util/ResponseMessage.java
+++ bidding-system/src/main/java/com/cars24/biddingsystem/util/ResponseMessage.java
@@ -9,7 +9,9 @@ public enum ResponseMessage {
     AUCTION_NOT_FOUND("M0003", "Auction not found"),
     INVALID_USER("M0004", "User not logged in or invalid user"),
     SUCCESS("M0005", "SUCCESS"),
-    FAILURE("M0006", "FAILURE");
+    FAILURE("M0006", "FAILURE"),
+    AUCTION_NOT_RUNNING_OR_PLACE_BID_IS_INVALID("M0007", "Either auction is not in 'RUNNING' state" +
+            " or may be place bid is smaller than the current bid for a given item code");
 
     private final String code;
     private final String message;
